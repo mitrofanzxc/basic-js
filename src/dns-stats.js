f66bbf86452ed2a7bbc3bea74bcf960de3fceb6a
mitrofanzxc <mitrofanzxc@gmail.com>
2022-05-03 01:48:07 +0300

    feat: add solutions for tasks

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let obj = {};
+  for (let el of domains) {
+    let arr = el.split(".").reverse();
+    let dom = "";
+    for (let el of arr) {
+      dom += "." + el;
+      if (obj.hasOwnProperty(dom)) {
+        obj[dom]++;
+      } else {
+        obj[dom] = 1;
+      }
+    }
+  }
+  return obj;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
